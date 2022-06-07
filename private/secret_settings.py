773e44b642a438abf518bafa55ee8d16dc0a5b63
yoshisada <ryan@ipavilion.com>
2022-06-06 20:09:43 -0700

    Delete private directory

--- private/secret_settings.py
+++
@@ -1,6 +0,0 @@
-OAUTH2GOOGLE_CLIENT_ID = "83822697836-m57jei0v374n9u1m1qlcido77q1mp92b.apps.googleusercontent.com"
-OAUTH2GOOGLE_CLIENT_SECRET = "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"
-DB_NAME = 'db'
-DB_USER = 'appuser'
-DB_PASSWORD = 'password'
-DB_CONNECTION="clp-183:us-west2:clp-183-db-1"
